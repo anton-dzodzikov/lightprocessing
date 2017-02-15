2f2b60407eaa4690021b91ab6a6bd33cb29f5d6b
Anton Dzodzikov <mr.dzodzikov@gmail.com>
2017-02-15 22:47:48 +1000

    Implement type reader

--- src/main/groovy/com/cardpay/backoffice/service/operations/repository/reader/OperationTypeReader.groovy
+++ src/main/groovy/com/cardpay/backoffice/service/operations/repository/reader/OperationTypeReader.groovy
@@ -1,15 +1,40 @@
 package com.cardpay.backoffice.service.operations.repository.reader
 
 import com.cardpay.backoffice.service.operations.repository.domain.OperationType
+import groovy.json.JsonSlurper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Component
 
 @Component
 class OperationTypeReader {
+    @Autowired
+    ResourceLoader resourceLoader
+
     Map<String, OperationType> readTypesFromFilesystem() {
-        [
-                "ONE": new OperationType(),
-                "TWO": new OperationType(),
-                "THREE": new OperationType()
-        ]
+        resourceLoader.getResource("classpath:operation-types").file.listFiles()
+                .collectEntries {[ it.name, buildType(it) ]}
+    }
+
+    // TODO: Validate that folder contents all files named right
+    OperationType buildType(File typeFolder) {
+        typeFolder.listFiles()
+                .find { it.name == "properties.json" }
+                .getText()
+                .with { new JsonSlurper().parseText(it) as Map }
+                .with { properties -> [
+                    name: typeFolder.name,
+                    description: properties.description,
+                    language: properties.language,
+                    executable: properties.executable as Boolean,
+                    restartable: properties.restartable as Boolean,
+                    enrollScript: typeFolder.listFiles()
+                            .find { it.name == "enroll.groovy" }
+                            .getText(),
+                    rollbackScript: typeFolder.listFiles()
+                            .find { it.name == "rollback.groovy" }
+                            .getText()
+                ]}
+                .with { new OperationType(it) }
     }
 }
