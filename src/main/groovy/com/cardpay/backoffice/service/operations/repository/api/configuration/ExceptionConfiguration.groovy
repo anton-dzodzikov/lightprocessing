4d76ec28f3edc9d1717e05050fc57cef1a389fe1
Anton Dzodzikov <mr.dzodzikov@gmail.com>
2017-02-15 22:55:54 +1000

    Fix exception handling

--- src/main/groovy/com/cardpay/backoffice/service/operations/repository/api/configuration/ExceptionConfiguration.groovy
+++ src/main/groovy/com/cardpay/backoffice/service/operations/repository/api/configuration/ExceptionConfiguration.groovy
@@ -2,9 +2,13 @@ package com.cardpay.backoffice.service.operations.repository.api.configuration
 
 import com.cardpay.backoffice.service.operations.repository.api.response.ExceptionResult
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
 
 @ControllerAdvice
-class ExceptionHandler {
+class ExceptionConfiguration {
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
     ExceptionResult defaultErrorHandler(Exception e) {
         [ body: e.message ]
     }
