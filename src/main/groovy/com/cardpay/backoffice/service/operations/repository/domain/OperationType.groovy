4653cfcc8deca3761577ffe6b878561f40d43a4d
Anton Dzodzikov <mr.dzodzikov@gmail.com>
2017-02-15 20:40:31 +1000

    Implement domain class

---
+++ src/main/groovy/com/cardpay/backoffice/service/operations/repository/domain/OperationType.groovy
@@ -0,0 +1,11 @@
+package com.cardpay.backoffice.service.operations.repository.domain
+
+class OperationType {
+    String name
+    String description
+    String enrollScript
+    String rollbackScript
+    String language
+    Boolean executable
+    Boolean restartable
+}
